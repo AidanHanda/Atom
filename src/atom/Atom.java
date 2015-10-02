926824db3390135707f838d4835a37019150beed
JasonBourne685 <akhanda@NN121-HA-05.HS.sd203.org>
2015-10-02 14:52:34 -0500

    InitCommit
    InitCommit

---
+++ src/atom/Atom.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package atom;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ *
+ * @author akhanda
+ */
+public class Atom extends JavaPlugin{
+
+    
+    @Override
+    public void onEnable(){
+        
+    }
+    
+    @Override
+    public void onDisable() {
+        
+    }
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
